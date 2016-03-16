df6ec5cf5b56620f197595478f28997ab8242438
Howard Abrams <howard.abrams@gmail.com>
2016-03-16 06:57:35 -0700

    Update prose and code for the workshop
    Mainly changed the instructions.org to look better if loaded in Emacs,
    and am not worrying about how it looks when rendered to HTML.

--- workshops/org-mode-litp/for-the-host.el
+++ workshops/org-mode-litp/for-the-host.el
@@ -53,10 +53,24 @@
 ;; ----------------------------------------------------------------------
 ;;  Create each function that represents a "presentation frame"
 
+(defun org-mode-intro-pdx-emacs-display ()
+  "Display the PDX Emacs Hackers logo and agenda."
+  (interactive)
+  (let ((parent (file-name-directory (buffer-file-name))))
+    (demo-it-frame-fullscreen)
+    (delete-other-windows)
+    (find-file "../../support/pdx-emacs.png")
+    (demo-it-hide-mode-line)
+    (split-window)
+    (other-window 1)
+    (demo-it-presentation (concat parent "agenda.org") 2)
+    (split-window-right)
+    (other-window 1)
+    (demo-it-presentation (concat parent "requirements.org") 2)))
+
 (defun org-mode-intro-load-presentation ()
   "Display for-the-host.org (an 'org-mode' file) as a presentation."
   ;; After the demonstration, we'll restore the original state
-  (demo-it-frame-fullscreen)
   (delete-other-windows)
   (demo-it-presentation "instructions.org"))
 
@@ -65,9 +79,26 @@
   (interactive)
   ;; If we switch to the original sandbox file, we can replace that
   ;; buffer window with a new one.
-  (pop-to-buffer "sandbox.org")
-  (demo-it-load-file "sandbox.org" 'side 2)
-  )
+  ;;       (pop-to-buffer "sandbox.org")
+  (when (require 'mwe-log-commands nil t)
+    (split-window-right)
+    (add-to-list 'mwe:*log-command-exceptions* 'org-self-insert-command)
+    (add-to-list 'mwe:*log-command-exceptions* 'org-delete-backward-char)
+    (add-to-list 'mwe:*log-command-exceptions* 'company-ignore)
+    (add-to-list 'mwe:*log-command-exceptions* 'ha/return-indent)
+    (mwe:open-command-log-buffer t)
+    (split-window-below)
+    (enlarge-window 10))
+  (let ((parent (file-name-directory (buffer-file-name))) )
+    (demo-it-load-file (concat parent "sandbox.org") nil 2)
+    (wrap-region-mode -1)
+    (org-bullets-mode -1)
+    (face-remap-add-relative 'org-level-4 '((:family "Source Code Pro" :height 148 :foreground "chocolate1")))
+    (face-remap-add-relative 'org-level-3 '((:family "Source Code Pro" :height 148 :foreground "Cyan1")))
+    (face-remap-add-relative 'org-level-2 '((:family "Source Code Pro" :height 148 :foreground "LightGoldenrod")))
+    (face-remap-add-relative 'org-level-1 '((:family "Source Code Pro" :height 148 :foreground "LightSkyBlue")))
+    (mwe:log-keyboard-commands)
+    (delete-region (point-min) (point-max))))
 
 ;; ----------------------------------------------------------------------
 ;; Demonstration and/or Presentation Order
@@ -82,7 +113,8 @@ next file to edit."
   (define-key demo-it-mode-adv-map (kbd "C-<f12>") 'demo-it-step)
   (define-key demo-it-mode-adv-map (kbd "<f12>") 'demo-it-presentation-advance)
 
-  (demo-it-start (list 'org-mode-intro-load-presentation
+  (demo-it-start (list 'org-mode-intro-pdx-emacs-display
+                       'org-mode-intro-load-presentation
                        'org-mode-intro-load-part-2) t))
 
 ;; ----------------------------------------------------------------------
